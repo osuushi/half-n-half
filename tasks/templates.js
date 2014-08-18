7fcb8514a6a606cef0a51b4fe2d73bf5c660d82f
Spencer Cohen <sc@moosoftware.com>
2014-08-17 20:50:00 -0700

    fix templates task

--- tasks/templates.js
+++ tasks/templates.js
@@ -1,10 +1,12 @@
 var fs = require('fs');
+var _ = require('lodash');
 
 module.exports = function (grunt) {
   grunt.registerTask('templates', function () {
     var templates = fs.readdirSync('templates').map(function (fileName) {
-      return fs.readFileSync('templates/' + fileName, 'utf8');
+      var name = fileName.split('.')[0];
+      return [name, fs.readFileSync('templates/' + fileName, 'utf8')];
     });
-    fs.writeFileSync('gen/templates.json', JSON.stringify(templates));
+    fs.writeFileSync('gen/templates.json', JSON.stringify(_.zipObject(templates)));
   });
 };
